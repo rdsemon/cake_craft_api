d631784348225efd7598c98f6e3e3de21d698bd8
rdsemon <codewithemons@gmail.com>
2026-05-01 22:41:02 +0600

    feat(app.ts): use morgan and express.json middleware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import morgan from "morgan";
+import cakeRouter from "./routes/cake.routes";
 const app = express();
 
-app.get("/", (req, res, next) => {
-  res.send("Hello from the api");
-});
+app.use(express.json({ limit: "120kb" }));
+app.use(morgan("dev"));
+
+app.use("/api/v1", cakeRouter);
 
 export default app;
