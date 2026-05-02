a18bc3e75c91394000ecec210238a35622663428
rdsemon <codewithemons@gmail.com>
2026-05-02 21:47:47 +0600

    feat(user.model.ts): create usersTable model

---
+++ src/models/user.model.ts
@@ -0,0 +1,13 @@
+import { pgTable, varchar, uuid, pgEnum } from "drizzle-orm/pg-core";
+
+const userEnum = pgEnum("user_roles", ["customer", "admin"]);
+
+const usersTable = pgTable("users", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  name: varchar("name", { length: 23 }).notNull(),
+  role: userEnum("role").notNull().default("customer"),
+  email: varchar("email", { length: 150 }).notNull().unique(),
+  password: varchar("password", { length: 255 }).notNull(),
+});
+
+export { usersTable, userEnum };
