0394781eb35db22615934b46897c233df212d5c9
rdsemon <codewithemons@gmail.com>
2026-05-03 21:34:11 +0600

    feat: implement zodValidator to validate inputs data

---
+++ src/middlewares/zodValidator.ts
@@ -0,0 +1,27 @@
+import type { Request, Response, NextFunction } from "express";
+import AppError from "../utils/AppError";
+
+const validateInput =
+  (schema: any) => (req: Request, res: Response, next: NextFunction) => {
+    const result = schema.safeParse({
+      body: req.body,
+      params: req.params,
+    });
+
+    if (!result.success) {
+      const errorMessage = result.error.issues
+        .map((el: any) => el.message)
+        .join(" , ");
+
+      return next(new AppError(errorMessage, 400));
+    }
+
+    const { body, params } = result.data;
+
+    req.body = body;
+    req.params = params;
+
+    next();
+  };
+
+export default validateInput;
