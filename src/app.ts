156fd13a579ae1c651a2a4d17b7c055cc9d008d6
rdsemon <codewithemons@gmail.com>
2026-05-03 21:37:25 +0600

    feat: connect allRouteError middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import morgan from "morgan";
 import cakeRouter from "./routes/cake.routes";
+import authRouter from "./routes/auth.routes";
+import allRouteError from "./middlewares/catchRouteError";
 const app = express();
 
 app.use(express.json({ limit: "120kb" }));
 app.use(morgan("dev"));
 
 app.use("/api/v1", cakeRouter);
+app.use("/api/v1/auth", authRouter);
+app.use(allRouteError);
 
 export default app;
