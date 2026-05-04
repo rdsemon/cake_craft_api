4dee3c1b027ec54388bd4d3d6e67c11eb976e5a8
rdsemon <codewithemons@gmail.com>
2026-05-04 22:26:14 +0600

    feat: implement createHashPass and comparePass functions

---
+++ src/utils/passwordGenerator.ts
@@ -0,0 +1,15 @@
+import bcrypt from "bcryptjs";
+import AppError from "./AppError";
+
+export const createHashPass = async (password: string) => {
+  const solt = Number(process.env.SOLTROUNDS);
+  if (!password) {
+    throw new AppError("password is required", 400);
+  }
+  const hashPass = await bcrypt.hash(password, solt);
+
+  return hashPass;
+};
+
+export const comparePass = async (dbPass: string, givenPass: string) =>
+  await bcrypt.compare(dbPass, givenPass);
