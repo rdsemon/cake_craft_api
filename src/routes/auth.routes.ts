2d6be076770187504a33a6c3a957a482836ba96b
rdsemon <codewithemons@gmail.com>
2026-05-04 22:21:56 +0600

    feat(auth.routes.ts): add  login route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,10 @@
 import expres from "express";
-import { signUp } from "../controllers/auth.controller";
+import { signUp, login } from "../controllers/auth.controller";
 import validateInput from "../middlewares/zodValidator";
 import { signUpSchema, loginSchema } from "../zodSchema/auth.schema";
 
 const router = expres.Router();
 
 router.post("/signUp", validateInput(signUpSchema), signUp);
-
+router.post("/login", validateInput(loginSchema), login);
 export default router;
