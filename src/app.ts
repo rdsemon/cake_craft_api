0c0e0ea88767b45e84a19c6bbc77c852b544bcc4
rdsemon <codewithemons@gmail.com>
2026-05-04 22:24:24 +0600

    feat(app):use cookie parser middleware connect the authRoute

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,20 @@ import express from "express";
 import morgan from "morgan";
 import cakeRouter from "./routes/cake.routes";
 import authRouter from "./routes/auth.routes";
+import userRouter from "./routes/user.routes";
 import allRouteError from "./middlewares/catchRouteError";
+import handleGlobalError from "./controllers/error.controller";
+import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(express.json({ limit: "120kb" }));
+app.use(cookieParser());
 app.use(morgan("dev"));
 
 app.use("/api/v1", cakeRouter);
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1", userRouter);
 app.use(allRouteError);
+app.use(handleGlobalError);
 
 export default app;
