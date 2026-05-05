7414c6003b62fe67c26d8869039807621fc1ad27
rdsemon <codewithemons@gmail.com>
2026-05-05 23:00:04 +0600

    refactor(sendJwt): change generateJwtToken logic

--- src/utils/sendJwt.ts
+++ src/utils/sendJwt.ts
@@ -2,21 +2,22 @@ import jwt from "jsonwebtoken";
 import type { SignOptions } from "jsonwebtoken";
 import AppError from "./AppError";
 
+const secretKey = process.env.JWT_SECRET;
+const expiresIn = process.env.EXPIRESIN;
+
+if (!secretKey || !expiresIn) {
+  throw new AppError(
+    "JWT configuration missing: secret or expiresIn not defined",
+    500,
+  );
+}
+
 export const generateJwtToken = (userId: string): string => {
-  const secretKey = process.env.JWT_SECRET;
-
-  const expiresIn = process.env.EXPIRESIN;
-
-  if (!secretKey || !expiresIn) {
-    throw new AppError(
-      "JWT configuration missing: secret or expiresIn not defined",
-      500,
-    );
-  }
-
   const token = jwt.sign({ userId }, secretKey, {
     expiresIn: expiresIn as SignOptions["expiresIn"],
   });
 
   return token;
 };
+
+export const verifyToken = (token: string) => jwt.verify(token, secretKey);
