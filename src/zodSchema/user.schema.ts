1f751b911ab0114001812c00ddd712978a30c03f
rdsemon <codewithemons@gmail.com>
2026-05-10 23:30:50 +0600

    feat: add two new validation field image , publicId

--- src/zodSchema/user.schema.ts
+++ src/zodSchema/user.schema.ts
@@ -6,8 +6,11 @@ export const updateUserSchema = z.object({
       .string()
       .min(3, { error: "name must be longer then 3 characters" })
       .max(20, { error: "name shoud not be longer then 20 characters" })
-      .trim(),
-    email: z.email(),
+      .trim()
+      .optional(),
+    email: z.email().optional(),
+    image: z.string().optional(),
+    publicId: z.string().optional(),
   }),
 });
 
