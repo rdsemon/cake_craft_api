0d2ec6bda65c36e33c4946acd8e50f56b2318b90
rdsemon <codewithemons@gmail.com>
2026-05-12 23:13:29 +0600

    feat(cake.model): implement cakes model using drizzle

---
+++ src/models/cake.model.ts
@@ -0,0 +1,9 @@
+import { pgTable, uuid, varchar, numeric } from "drizzle-orm/pg-core";
+
+export const cakeTable = pgTable("cakes", {
+  id: uuid("id").primaryKey().defaultRandom(),
+
+  title: varchar("title", { length: 250 }).notNull(),
+
+  price: numeric("price", { precision: 10, scale: 2 }).notNull(),
+});
