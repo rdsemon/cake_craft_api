c6921ab8a6de9b6c0bc50013f0566c3f2c6abfd7
rdsemon <codewithemons@gmail.com>
2026-05-12 21:48:03 +0600

    feat(user.schema): add default image in image field

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -14,7 +14,11 @@ const usersTable = pgTable("users", {
   name: varchar("name", { length: 23 }).notNull(),
   role: userEnum("role").notNull().default("customer"),
   email: varchar("email", { length: 150 }).notNull().unique(),
-  image: varchar("image", { length: 500 }).$type<string | null>().default(null),
+  image: varchar("image", { length: 500 })
+    .$type<string>()
+    .default(
+      "https://res.cloudinary.com/dz6gmlrjf/image/upload/v1778598812/847969_eyatxx.png",
+    ),
   publicId: text("public_id"),
   password: varchar("password", { length: 255 }).notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
