e436e2bf79a4f2ddf56b85d02711a2a238d36fc9
rdsemon <codewithemons@gmail.com>
2026-05-13 22:25:37 +0600

    feat(cake.routes): configure cake api routes

--- src/routes/cake.routes.ts
+++ src/routes/cake.routes.ts
@@ -2,8 +2,22 @@ import express from "express";
 
 const router = express.Router();
 
-import { getAllCakes } from "../controllers/cake.controller";
+import {
+  getAllCakes,
+  getOneCakeById,
+  createCake,
+  updateCakeInfo,
+  deleteCakeById,
+  deleteAllCakes,
+} from "../controllers/cake.controller";
 
-router.get("/cake", getAllCakes);
+router.route("/cakes").get(getAllCakes).post(createCake);
+router
+  .route("/cakes/:id")
+  .get(getOneCakeById)
+  .patch(updateCakeInfo)
+  .delete(deleteCakeById);
+
+router.delete("/cakes/delete", deleteAllCakes);
 
 export default router;
