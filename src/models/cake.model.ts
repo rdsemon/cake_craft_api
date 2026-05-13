5f3dd5bd694d4634f0785336dbf16ca31fc47079
rdsemon <codewithemons@gmail.com>
2026-05-13 21:46:59 +0600

    feat(cake.model): add new fields in model image, publicId and createdAt

--- src/models/cake.model.ts
+++ src/models/cake.model.ts
@@ -1,11 +1,21 @@
-import { pgTable, uuid, varchar, numeric } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  uuid,
+  varchar,
+  numeric,
+  timestamp,
+  integer,
+  text,
+} from "drizzle-orm/pg-core";
 
 export const cakeTable = pgTable("cakes", {
   id: uuid("id").primaryKey().defaultRandom(),
-
   title: varchar("title", { length: 250 }).notNull(),
-
   price: numeric("price", { precision: 10, scale: 2 }).notNull(),
+  quantity: integer("quantity").default(1),
+  image: varchar("image", { length: 500 }).$type<string | null>().default(null),
+  publicId: text("public_id"),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
 });
 
 export default cakeTable;
