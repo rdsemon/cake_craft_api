b5a5e86da56cd307f91351bf0d5b10466178a2cb
rdsemon <codewithemons@gmail.com>
2026-05-14 15:12:19 +0600

    refactor(cake.route): validate createCake and updateCake route

--- src/routes/cake.routes.ts
+++ src/routes/cake.routes.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { createCakeSchema, updateCakeSchema } from "../zodSchema/cake.schema";
+import validateInput from "../middlewares/zodValidator";
 
 const router = express.Router();
 
@@ -11,11 +13,14 @@ import {
   deleteAllCakes,
 } from "../controllers/cake.controller";
 
-router.route("/cakes").get(getAllCakes).post(createCake);
+router
+  .route("/cakes")
+  .get(getAllCakes)
+  .post(validateInput(createCakeSchema), createCake);
 router
   .route("/cakes/:id")
   .get(getOneCakeById)
-  .patch(updateCakeInfo)
+  .patch(validateInput(updateCakeSchema), updateCakeInfo)
   .delete(deleteCakeById);
 
 router.delete("/cakes/delete", deleteAllCakes);
