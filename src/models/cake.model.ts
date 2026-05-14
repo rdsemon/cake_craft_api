dcc720dbd9302d980a15876802753a2aade8ea82
rdsemon <codewithemons@gmail.com>
2026-05-14 10:31:20 +0600

    feat(cake.model): add some new fields

--- src/models/cake.model.ts
+++ src/models/cake.model.ts
@@ -3,19 +3,51 @@ import {
   uuid,
   varchar,
   numeric,
-  timestamp,
   integer,
   text,
+  timestamp,
+  check,
 } from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
 
-export const cakeTable = pgTable("cakes", {
-  id: uuid("id").primaryKey().defaultRandom(),
-  title: varchar("title", { length: 250 }).notNull(),
-  price: numeric("price", { precision: 10, scale: 2 }).notNull(),
-  quantity: integer("quantity").default(1),
-  image: varchar("image", { length: 500 }).$type<string | null>().default(null),
-  publicId: text("public_id"),
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-});
+export const cakeTable = pgTable(
+  "cakes",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+
+    title: varchar("title", { length: 250 }).notNull(),
+
+    description: varchar("description", { length: 1000 }).notNull(),
+
+    price: numeric("price", {
+      precision: 10,
+      scale: 2,
+    }).notNull(),
+
+    quantity: integer("quantity").default(1).notNull(),
+
+    image: varchar("image", { length: 500 })
+      .$type<string | null>()
+      .default(null),
+
+    publicId: text("public_id"),
+
+    createdAt: timestamp("created_at", {
+      withTimezone: true,
+    })
+      .defaultNow()
+      .notNull(),
+
+    updatedAt: timestamp("updated_at", {
+      withTimezone: true,
+    })
+      .defaultNow()
+      .notNull(),
+  },
+  (table) => [
+    check("price_positive", sql`${table.price} > 0`),
+    check("quantity_positive", sql`${table.quantity} >= 0`),
+  ],
+);
 
 export default cakeTable;
