c5a2e8fb423d5fa9ead4b12c19edbd9bb9299a37
rdsemon <codewithemons@gmail.com>
2026-05-14 15:14:28 +0600

    feat(cake.model): add new fields inStock , isAvailable in database

--- src/models/cake.model.ts
+++ src/models/cake.model.ts
@@ -6,48 +6,44 @@ import {
   integer,
   text,
   timestamp,
-  check,
+  boolean,
 } from "drizzle-orm/pg-core";
-import { sql } from "drizzle-orm";
 
-export const cakeTable = pgTable(
-  "cakes",
-  {
-    id: uuid("id").primaryKey().defaultRandom(),
+export const cakeTable = pgTable("cakes", {
+  id: uuid("id").primaryKey().defaultRandom(),
 
-    title: varchar("title", { length: 250 }).notNull(),
+  title: varchar("title", { length: 250 }).notNull(),
 
-    description: varchar("description", { length: 1000 }).notNull(),
+  description: varchar("description", { length: 1000 }).notNull(),
 
-    price: numeric("price", {
-      precision: 10,
-      scale: 2,
-    }).notNull(),
+  price: numeric("price", {
+    precision: 10,
+    scale: 2,
+  }).notNull(),
+  quantity: integer("quantity").default(1).notNull(),
 
-    quantity: integer("quantity").default(1).notNull(),
+  inStock: integer("in_stock").default(1).notNull(),
 
-    image: varchar("image", { length: 500 })
-      .$type<string | null>()
-      .default(null),
+  isAvailable: boolean("is_available").default(true).notNull(),
 
-    publicId: text("public_id"),
+  coverImage: varchar("cover_image", { length: 500 })
+    .$type<string | null>()
+    .default(null),
 
-    createdAt: timestamp("created_at", {
-      withTimezone: true,
-    })
-      .defaultNow()
-      .notNull(),
+  publicId: text("public_id"),
 
-    updatedAt: timestamp("updated_at", {
-      withTimezone: true,
-    })
-      .defaultNow()
-      .notNull(),
-  },
-  (table) => [
-    check("price_positive", sql`${table.price} > 0`),
-    check("quantity_positive", sql`${table.quantity} >= 0`),
-  ],
-);
+  createdAt: timestamp("created_at", {
+    withTimezone: true,
+  })
+    .defaultNow()
+    .notNull(),
+
+  updatedAt: timestamp("updated_at", {
+    withTimezone: true,
+  })
+    .defaultNow()
+    .$onUpdate(() => new Date())
+    .notNull(),
+});
 
 export default cakeTable;
