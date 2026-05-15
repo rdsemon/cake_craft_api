60ad6708326eea0a14616dcf8e85340ba9b10c77
rdsemon <codewithemons@gmail.com>
2026-05-15 22:06:55 +0600

    feat(cake.schema): implement updateCakeSchmea and export it as a type

--- src/zodSchema/cake.schema.ts
+++ src/zodSchema/cake.schema.ts
@@ -25,37 +25,42 @@ export const createCakeSchema = z.object({
 });
 
 export const updateCakeSchema = z.object({
-  body: z.object({
-    title: z
-      .string()
-      .min(5, { error: "Title must be longer than 5 characters" })
-      .max(100, { error: "Title should not be longer than 100 characters" })
-      .trim()
-      .optional(),
+  body: z
+    .object({
+      title: z
+        .string()
+        .min(5, { error: "Title must be longer than 5 characters" })
+        .max(100, { error: "Title should not be longer than 100 characters" })
+        .trim()
+        .optional(),
 
-    description: z
-      .string()
-      .min(6, { error: "Description must be longer than 20 characters" })
-      .trim()
-      .optional(),
+      description: z
+        .string()
+        .min(6, { error: "Description must be longer than 20 characters" })
+        .trim()
+        .optional(),
 
-    price: z
-      .number()
-      .positive({ error: "Price must be greater than 0" })
-      .optional(),
+      price: z
+        .number()
+        .positive({ error: "Price must be greater than 0" })
+        .optional(),
 
-    inStock: z
-      .number()
-      .int({ error: "In stock must be an integer" })
-      .nonnegative({ error: "In stock cannot be negative" })
-      .optional(),
+      inStock: z
+        .number()
+        .int({ error: "In stock must be an integer" })
+        .nonnegative({ error: "In stock cannot be negative" })
+        .optional(),
 
-    coverImage: z.url().optional().optional(),
-  }),
+      coverImage: z.url().optional().optional(),
+    })
+    .refine((data) => Object.keys(data).length > 0, {
+      message: "At least one field must be provided",
+    }),
 
   params: z.object({
-    id: z.string(),
+    id: z.uuid(),
   }),
 });
 
 export type CreateCakeBody = z.infer<typeof createCakeSchema>["body"];
+export type UpdateCakeBody = z.infer<typeof updateCakeSchema>["body"];
