29a5b61c86b8a7bf61c3cee0bd0912b76f8dc833
rdsemon <codewithemons@gmail.com>
2026-05-15 21:55:40 +0600

    refactor(cake.controller): replace the database logic

--- src/controllers/cake.controller.ts
+++ src/controllers/cake.controller.ts
@@ -1,95 +1,51 @@
 import asyncHandler from "../utils/asyncHandler";
-import AppError from "../utils/AppError";
-import db from "../database";
-import cakeTable from "../models/cake.model";
-import { eq } from "drizzle-orm";
+import {
+  getAllCakesService,
+  getOneCakeService,
+  createCakeService,
+  deleteCakeService,
+  updateCakeService,
+  deleteAllCakesService,
+} from "../services/dbService/cakeDb.services";
 
 export const getAllCakes = asyncHandler(async (req, res, next) => {
-  const cakes = await db.select().from(cakeTable);
-
+  const cakes = await getAllCakesService();
   res
     .status(200)
     .json({ status: "successful", total: cakes.length, data: cakes });
 });
 
 export const createCake = asyncHandler(async (req, res, next) => {
-  const [newCake] = await db
-    .insert(cakeTable)
-    .values(req.body)
-    .returning({ id: cakeTable.id });
-
-  if (!newCake?.id) {
-    return next(new AppError("Uploading cake fail", 400));
-  }
+  const cake = await createCakeService(req.body);
   res.status(201).json({
     status: "successful",
-    message: `Upload cake successful id:${newCake.id} `,
+    message: `Upload cake successful id:${cake.id} `,
   });
 });
 
 export const getOneCakeById = asyncHandler(async (req, res, next) => {
   const id = req.params.id as string;
-
-  const cake = await db
-    .select()
-    .from(cakeTable)
-    .where(eq(cakeTable.id, id))
-    .limit(1);
-
-  if (!cake || cake.length === 0) {
-    return next(new AppError("Cake not found", 404));
-  }
-
+  const cake = await getOneCakeService(id);
   res.status(200).json({ status: "successful", data: cake });
 });
 
 export const updateCakeInfo = asyncHandler(async (req, res, next) => {
   const id = req.params.id as string;
-
-  const cake = await db
-    .select()
-    .from(cakeTable)
-    .where(eq(cakeTable.id, id))
-    .limit(1);
-
-  if (!cake || cake.length === 0) {
-    return next(new AppError("Cake not found", 404));
-  }
-
-  const updatedCake = await db
-    .update(cakeTable)
-    .set(req.body)
-    .where(eq(cakeTable.id, id))
-    .returning({ id: cakeTable.id });
-
-  if (!updatedCake || updatedCake.length === 0) {
-    return next(new AppError("Update fail", 400));
-  }
-
+  await updateCakeService(id, req.body);
   res.status(200).json({ status: "successful", message: "update successfull" });
 });
 
 export const deleteCakeById = asyncHandler(async (req, res, next) => {
   const id = req.params.id as string;
+  //checking if the cake data exist
+  await getOneCakeService(id);
+  //delete cake
+  await deleteCakeService(id);
 
-  const cake = await db
-    .select()
-    .from(cakeTable)
-    .where(eq(cakeTable.id, id))
-    .limit(1);
-
-  if (!cake || cake.length === 0) {
-    return next(new AppError("Cake not found", 404));
-  }
-
-  await db.delete(cakeTable).where(eq(cakeTable.id, id));
-
-  res
-    .status(204)
-    .json({ status: "successful", message: "Deleting successful" });
+  res.status(204).send();
 });
 
 export const deleteAllCakes = asyncHandler(async (req, res, next) => {
-  await db.delete(cakeTable);
-  res.status(204).json({ status: "successful" });
+  await deleteAllCakesService();
+  res.status(204).send();
 });
