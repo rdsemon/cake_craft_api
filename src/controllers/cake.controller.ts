b429760043593eb8a9f4e83ce4d6316967b454fe
rdsemon <codewithemons@gmail.com>
2026-05-16 14:47:25 +0600

    refactor(cake.controller): remove unnecessary next form the functions

--- src/controllers/cake.controller.ts
+++ src/controllers/cake.controller.ts
@@ -8,14 +8,14 @@ import {
   deleteAllCakesService,
 } from "../services/dbService/cakeDb.services";
 
-export const getAllCakes = asyncHandler(async (req, res, next) => {
+export const getAllCakes = asyncHandler(async (req, res) => {
   const cakes = await getAllCakesService();
   res
     .status(200)
     .json({ status: "successful", total: cakes.length, data: cakes });
 });
 
-export const createCake = asyncHandler(async (req, res, next) => {
+export const createCake = asyncHandler(async (req, res) => {
   const cake = await createCakeService(req.body);
   res.status(201).json({
     status: "successful",
@@ -23,19 +23,20 @@ export const createCake = asyncHandler(async (req, res, next) => {
   });
 });
 
-export const getOneCakeById = asyncHandler(async (req, res, next) => {
-  const id = req.params.id as string;
+export const getOneCakeById = asyncHandler(async (req, res) => {
+  const id = String(req.params.id);
+
   const cake = await getOneCakeService(id);
   res.status(200).json({ status: "successful", data: cake });
 });
 
-export const updateCakeInfo = asyncHandler(async (req, res, next) => {
-  const id = req.params.id as string;
+export const updateCakeInfo = asyncHandler(async (req, res) => {
+  const id = String(req.params.id);
   await updateCakeService(id, req.body);
   res.status(200).json({ status: "successful", message: "update successfull" });
 });
 
-export const deleteCakeById = asyncHandler(async (req, res, next) => {
+export const deleteCakeById = asyncHandler(async (req, res) => {
   const id = req.params.id as string;
   //checking if the cake data exist
   await getOneCakeService(id);
@@ -45,7 +46,7 @@ export const deleteCakeById = asyncHandler(async (req, res, next) => {
   res.status(204).send();
 });
 
-export const deleteAllCakes = asyncHandler(async (req, res, next) => {
+export const deleteAllCakes = asyncHandler(async (req, res) => {
   await deleteAllCakesService();
   res.status(204).send();
 });
