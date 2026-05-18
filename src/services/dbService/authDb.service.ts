b1bb82027dcaa7ef70888c2c3beaf2d04d953886
rdsemon <codewithemons@gmail.com>
2026-05-18 23:23:11 +0600

    refactor(authDb): return the user object instead of user array

--- src/services/dbService/authDb.service.ts
+++ src/services/dbService/authDb.service.ts
@@ -2,16 +2,23 @@ import { eq } from "drizzle-orm";
 import db from "../../database";
 import { usersTable } from "../../models/user.model";
 import type { signUpUserData } from "../../types/authType";
+import AppError from "../../utils/AppError";
 
 export const signUpService = async (userData: signUpUserData) => {
-  return await db
+  const [user] = await db
     .insert(usersTable)
     .values(userData)
     .returning({ id: usersTable.id });
+
+  if (!user?.id) {
+    throw new AppError("signUpd fail try agian", 401);
+  }
+
+  return user;
 };
 
 export const loginService = async (email: string) => {
-  return await db
+  const [user] = await db
     .select({
       password: usersTable.password,
       email: usersTable.email,
@@ -19,4 +26,10 @@ export const loginService = async (email: string) => {
     })
     .from(usersTable)
     .where(eq(usersTable.email, email));
+
+  if (!user?.email) {
+    throw new AppError("User dose not exist", 404);
+  }
+
+  return user;
 };
