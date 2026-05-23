aeb252f06d1c1c6a877663d48da1faafdc39f91b
rdsemon <codewithemons@gmail.com>
2026-05-23 22:38:19 +0600

    refactor(database): using pool to connect the database string

--- src/database.ts
+++ src/database.ts
@@ -1,5 +1,10 @@
 import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
-const db = drizzle(process.env.DATABASE_URL!);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const db = drizzle(pool);
 
 export default db;
