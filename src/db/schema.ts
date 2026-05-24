6deb6754d314d92ccbdfc6f480437204f77e2eb1
rdsemon <codewithemons@gmail.com>
2026-05-24 22:54:11 +0600

    feat(schema): add cart and cart_items model

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,5 @@
 import { userEnum, usersTable } from "../models/user.model";
 import cakeTable from "../models/cake.model";
+import { carts, cartItems } from "../models/cart.model";
 
-export { usersTable, userEnum, cakeTable };
+export { usersTable, userEnum, cakeTable, carts, cartItems };
