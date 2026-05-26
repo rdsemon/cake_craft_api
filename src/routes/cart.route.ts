e995d8dfa22c99773e97cfafe7498babca254605
rdsemon <codewithemons@gmail.com>
2026-05-26 21:59:08 +0600

    feat(cartRoute): configure the route for cart

---
+++ src/routes/cart.route.ts
@@ -0,0 +1,20 @@
+import express from "express";
+const router = express.Router();
+import {
+  addToCart,
+  getCart,
+  decreaseCartItemQuantity,
+  removeCartItem,
+  clearCart,
+} from "../controllers/cart.controller";
+import { protect } from "../controllers/auth.controller";
+
+router.use(protect);
+
+router.post("/cart", addToCart);
+router.get("/cart", getCart);
+router.patch("/cart/decrease", decreaseCartItemQuantity);
+router.delete("/cart/remove/:cakeId", removeCartItem);
+router.delete("/cart/clear", clearCart);
+
+export default router;
