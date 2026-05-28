0e364bce0fd7daf7c5d06c7a52420134370316f2
rdsemon <codewithemons@gmail.com>
2026-05-28 23:11:01 +0600

    feat(cartDbHelper): implement helper functions for cart service

--- src/services/cart/cartDb.helper.service.ts
+++ src/services/cart/cartDb.helper.service.ts
@@ -1,7 +1,8 @@
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import db from "../../database";
 import cakeTable from "../../models/cake.model";
 import AppError from "../../utils/AppError";
+import { cartItems, carts } from "../../models/cart.model";
 
 export const findCakeById = async (cakeId: string) => {
   const [cake] = await db
@@ -14,4 +15,45 @@ export const findCakeById = async (cakeId: string) => {
   }
 
   return cake;
+};
+
+export const findCartById = async (userId: string) => {
+  const [cart] = await db.select().from(carts).where(eq(carts.userId, userId));
+
+  if (!cart) {
+    throw new AppError("Cart not found", 404);
+  }
+
+  return cart;
+};
+
+export const checkExistingItem = async (cakeId: string) => {
+  const [existingItem] = await db
+    .select()
+    .from(cartItems)
+    .where(and(eq(cartItems.cartId, carts.id), eq(cartItems.cakeId, cakeId)));
+
+  if (!existingItem) {
+    throw new AppError("Item not found in cart", 404);
+  }
+
+  return existingItem;
+};
+
+export const updateCartItem = async (
+  price: number,
+  quantity: number,
+  cakeId: string,
+) => {
+  const [updatedItem] = await db
+    .update(cartItems)
+    .set({ price, quantity })
+    .where(and(eq(cartItems.cartId, carts.id), eq(cartItems.cakeId, cakeId)))
+    .returning();
+
+  if (!updatedItem) {
+    throw new AppError("Update fail", 400);
+  }
+
+  return updatedItem;
 };
