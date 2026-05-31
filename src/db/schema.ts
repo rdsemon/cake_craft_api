86198b8c0d72c6b2cd80bd5f0c993073fe6a0729
rdsemon <codewithemons@gmail.com>
2026-05-31 23:14:04 +0600

    feat(schema): add orders and orderItems model

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,14 @@
 import { userEnum, usersTable } from "../models/user.model";
 import cakeTable from "../models/cake.model";
 import { carts, cartItems } from "../models/cart.model";
+import { orders, orderItems } from "../models/order.model";
 
-export { usersTable, userEnum, cakeTable, carts, cartItems };
+export {
+  usersTable,
+  userEnum,
+  cakeTable,
+  carts,
+  cartItems,
+  orders,
+  orderItems,
+};
