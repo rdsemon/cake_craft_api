2b703b7e3fd13f63dd0706e91a1c08ba2886bf71
rdsemon <codewithemons@gmail.com>
2026-06-03 22:22:40 +0600

    feat(reviewModel): configure review model to get users review

---
+++ src/models/review.model.ts
@@ -0,0 +1,22 @@
+import { pgTable, uuid, integer, text, timestamp } from "drizzle-orm/pg-core";
+
+import { usersTable } from "./user.model";
+import cakeTable from "./cake.model";
+
+export const reviews = pgTable("reviews", {
+  id: uuid("id").defaultRandom().primaryKey(),
+
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => usersTable.id),
+
+  cakeId: uuid("cake_id")
+    .notNull()
+    .references(() => cakeTable.id),
+
+  rating: integer("rating").notNull(),
+
+  comment: text("comment"),
+
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
