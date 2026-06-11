a386da14e947a9f7fb1d9ced4f9c01efddc407b4
rdsemon <codewithemons@gmail.com>
2026-06-11 22:27:20 +0600

    refactor(cakeModel): add indexes for title full-text search and userId

--- src/models/cake.model.ts
+++ src/models/cake.model.ts
@@ -7,49 +7,61 @@ import {
   text,
   timestamp,
   boolean,
+  index,
 } from "drizzle-orm/pg-core";
 import { usersTable } from "./user.model.js";
+import { sql } from "drizzle-orm";
 
-export const cakeTable = pgTable("cakes", {
-  id: uuid("id").primaryKey().defaultRandom(),
+export const cakeTable = pgTable(
+  "cakes",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
 
-  title: varchar("title", { length: 250 }).notNull(),
+    title: varchar("title", { length: 250 }).notNull(),
 
-  description: varchar("description", { length: 1000 }).notNull(),
+    description: varchar("description", { length: 1000 }).notNull(),
 
-  price: numeric("price", {
-    precision: 10,
-    scale: 2,
-    mode: "number",
-  }).notNull(),
-  quantity: integer("quantity").default(1).notNull(),
+    price: numeric("price", {
+      precision: 10,
+      scale: 2,
+      mode: "number",
+    }).notNull(),
+    quantity: integer("quantity").default(1).notNull(),
 
-  inStock: integer("in_stock").default(1).notNull(),
+    inStock: integer("in_stock").default(1).notNull(),
 
-  isAvailable: boolean("is_available").default(true).notNull(),
+    isAvailable: boolean("is_available").default(true).notNull(),
 
-  coverImage: varchar("cover_image", { length: 500 })
-    .$type<string | null>()
-    .default(null),
+    coverImage: varchar("cover_image", { length: 500 })
+      .$type<string | null>()
+      .default(null),
 
-  publicId: text("public_id"),
+    publicId: text("public_id"),
 
-  userId: uuid("user_id")
-    .references(() => usersTable.id, { onDelete: "cascade" })
-    .notNull(),
+    userId: uuid("user_id")
+      .references(() => usersTable.id, { onDelete: "cascade" })
+      .notNull(),
 
-  createdAt: timestamp("created_at", {
-    withTimezone: true,
-  })
-    .defaultNow()
-    .notNull(),
+    createdAt: timestamp("created_at", {
+      withTimezone: true,
+    })
+      .defaultNow()
+      .notNull(),
 
-  updatedAt: timestamp("updated_at", {
-    withTimezone: true,
-  })
-    .defaultNow()
-    .$onUpdate(() => new Date())
-    .notNull(),
-});
+    updatedAt: timestamp("updated_at", {
+      withTimezone: true,
+    })
+      .defaultNow()
+      .$onUpdate(() => new Date())
+      .notNull(),
+  },
+  (table: any) => [
+    index("title_search_index").using(
+      "gin",
+      sql`to_tsvector('english', ${table.title})`,
+    ),
+    index("user_id_index").on(table.userId),
+  ],
+);
 
 export default cakeTable;
