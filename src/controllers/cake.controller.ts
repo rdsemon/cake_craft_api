dfcf06c36daa192f60e0c9147f014b49df5613c5
rdsemon <codewithemons@gmail.com>
2026-06-17 22:26:18 +0600

    refactor(cakeController): add type on options in getAllCakesService controller

--- src/controllers/cake.controller.ts
+++ src/controllers/cake.controller.ts
@@ -1,5 +1,6 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import type { Request, Response } from "express";
+import type { GetCakesOptions } from "../types/controllerTypes.js";
 import {
   getAllCakesService,
   getOneCakeService,
@@ -9,22 +10,19 @@ import {
   deleteAllCakesService,
 } from "../services/cake/cakeDb.service.js";
 
-
-export const getAllCakes = asyncHandler(async (req:Request, res:Response) => {
+export const getAllCakes = asyncHandler(async (req: Request, res: Response) => {
   const options = req.query;
-  const result = await getAllCakesService(options);
-  res
-    .status(200)
-    .json({
-      status: "successful",
-      total: result.cakes.length,
-      data: result.cakes,
-      page: result.page,
-      limit: result.limit,
-    });
+  const result = await getAllCakesService(options as GetCakesOptions);
+  res.status(200).json({
+    status: "successful",
+    total: result.cakes.length,
+    data: result.cakes,
+    page: result.page,
+    limit: result.limit,
+  });
 });
 
-export const createCake = asyncHandler(async (req:Request, res:Response) => {
+export const createCake = asyncHandler(async (req: Request, res: Response) => {
   const cake = await createCakeService(req.body);
   res.status(201).json({
     status: "successful",
@@ -32,30 +30,40 @@ export const createCake = asyncHandler(async (req:Request, res:Response) => {
   });
 });
 
-export const getOneCakeById = asyncHandler(async (req:Request, res:Response) => {
-  const id = String(req.params.id);
+export const getOneCakeById = asyncHandler(
+  async (req: Request, res: Response) => {
+    const id = String(req.params.id);
 
-  const cake = await getOneCakeService(id);
-  res.status(200).json({ status: "successful", data: cake });
-});
+    const cake = await getOneCakeService(id);
+    res.status(200).json({ status: "successful", data: cake });
+  },
+);
 
-export const updateCakeInfo = asyncHandler(async (req:Request, res:Response) => {
-  const id = String(req.params.id);
-  await updateCakeService(id, req.body);
-  res.status(200).json({ status: "successful", message: "update successfull" });
-});
+export const updateCakeInfo = asyncHandler(
+  async (req: Request, res: Response) => {
+    const id = String(req.params.id);
+    await updateCakeService(id, req.body);
+    res
+      .status(200)
+      .json({ status: "successful", message: "update successfull" });
+  },
+);
 
-export const deleteCakeById = asyncHandler(async (req:Request, res:Response) => {
-  const id = req.params.id as string;
-  //checking if the cake data exist
-  await getOneCakeService(id);
-  //delete cake
-  await deleteCakeService(id);
+export const deleteCakeById = asyncHandler(
+  async (req: Request, res: Response) => {
+    const id = req.params.id as string;
+    //checking if the cake data exist
+    await getOneCakeService(id);
+    //delete cake
+    await deleteCakeService(id);
 
-  res.status(204).send();
-});
+    res.status(204).send();
+  },
+);
 
-export const deleteAllCakes = asyncHandler(async (req:Request, res:Response) => {
-  await deleteAllCakesService();
-  res.status(204).send();
-});
+export const deleteAllCakes = asyncHandler(
+  async (req: Request, res: Response) => {
+    await deleteAllCakesService();
+    res.status(204).send();
+  },
+);
