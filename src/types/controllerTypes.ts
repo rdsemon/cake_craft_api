cae12a8c298593f30e064d92aca530f9ccb6500e
rdsemon <codewithemons@gmail.com>
2026-06-17 22:24:38 +0600

    feat(controllerTypes): write types for controller functions

---
+++ src/types/controllerTypes.ts
@@ -0,0 +1,27 @@
+import AppError from "../utils/AppError.js";
+import { ZodError } from "zod";
+
+//error controller types
+export type DatabaseError = Error & {
+  detail?: string;
+  column?: string;
+  cause?: { code: string };
+};
+
+export type ErrorType = (AppError | ZodError | DatabaseError | Error) & {
+  status?: string;
+  statusCode?: number;
+  message: string;
+  name?: string;
+};
+
+//getCakeController types
+
+export interface GetCakesOptions {
+  search?: string;
+  minPrice?: string | number;
+  maxPrice?: string | number;
+  sort?: "price-asc" | "price-desc" | "title";
+  page?: string | number;
+  limit?: string | number;
+}
