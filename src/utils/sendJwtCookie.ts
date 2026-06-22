89a232ce7e30037252e1901f512474da648ce391
rdsemon <codewithemons@gmail.com>
2026-06-22 22:27:46 +0600

    refactor(sendJwtCooke): change sameSite value from strict to none

--- src/utils/sendJwtCookie.ts
+++ src/utils/sendJwtCookie.ts
@@ -3,7 +3,7 @@ const sendJwtCooke = (res: Response, token: string) => {
   const cookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict" as const,
+    sameSite: "none" as const,
     maxAge: 7 * 24 * 60 * 60 * 1000,
   };
 
