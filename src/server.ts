5e62014590df81c82ea77086b1df2d67bbf8e16c
rdsemon <codewithemons@gmail.com>
2026-07-02 22:29:11 +0600

    fix: change the port option

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,7 @@ import "dotenv/config";
 import app from "./app.js";
 
 const server = app;
-
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
   console.log(`Listening on port ${port}`);
