c2748040f2a4b1e6de413fcf0b2611baa79073b0
rdsemon <codewithemons@gmail.com>
2026-07-09 22:53:27 +0600

    feat: add coverImage and publicId field

--- src/data/cake.data.ts
+++ src/data/cake.data.ts
@@ -2,9 +2,11 @@ export const cakesData = [
   {
     title: "Chocolate Truffle Cake",
     description: "Rich chocolate sponge layered with smooth truffle cream.",
+    coverImage:
+      "https://res.cloudinary.com/dz6gmlrjf/image/upload/v1783604519/Chocolate-Truffle-Cake_ahu8jt.jpg",
+    publicId: "Chocolate-Truffle-Cake_ahu8jt",
     price: 24.99,
     quantity: 5,
-
     isAvailable: true,
     userId: "2f4cff97-05fa-4a9d-8d75-0e9c4d07ab4d",
   },
@@ -14,7 +16,9 @@ export const cakesData = [
     description: "Soft vanilla sponge with whipped cream frosting.",
     price: 19.5,
     quantity: 4,
-
+    coverImage:
+      "https://res.cloudinary.com/dz6gmlrjf/image/upload/v1783604521/vanilla-dream_u7onjd.jpg",
+    publicId: "vanilla-dream_u7onjd",
     isAvailable: true,
     userId: "2f4cff97-05fa-4a9d-8d75-0e9c4d07ab4d",
   },
@@ -22,6 +26,9 @@ export const cakesData = [
   {
     title: "Red Velvet Cake",
     description: "Classic red velvet cake with cream cheese frosting.",
+    coverImage:
+      "https://res.cloudinary.com/dz6gmlrjf/image/upload/v1783604518/red-velvet-cake_kvmdhg.jpg",
+    publicId: "red-velvet-cake_kvmdhg",
     price: 29.99,
     quantity: 3,
 
@@ -33,9 +40,11 @@ export const cakesData = [
   {
     title: "Black Forest Cake",
     description: "Chocolate cake layered with cherries and whipped cream.",
+    coverImage:
+      "https://res.cloudinary.com/dz6gmlrjf/image/upload/v1783604517/black-froset-cake_tpj8o3.jpg",
+    publicId: "black-froset-cake_tpj8o3",
     price: 27.25,
     quantity: 2,
-
     isAvailable: true,
     userId: "2f4cff97-05fa-4a9d-8d75-0e9c4d07ab4d",
   },
@@ -45,7 +54,9 @@ export const cakesData = [
     description: "Fresh strawberry cream cake with fluffy sponge.",
     price: 22.0,
     quantity: 6,
-
+    coverImage:
+      "https://res.cloudinary.com/dz6gmlrjf/image/upload/v1783604524/strawberry-cake_gfnatd.jpg",
+    publicId: "strawberry-cake-01",
     isAvailable: true,
     userId: "2f4cff97-05fa-4a9d-8d75-0e9c4d07ab4d",
   },
@@ -53,6 +64,9 @@ export const cakesData = [
   {
     title: "Mango Bliss Cake",
     description: "Tropical mango flavored cake with mango glaze.",
+    coverImage:
+      "https://res.cloudinary.com/dz6gmlrjf/image/upload/v1783604515/mango-bless-cake_twurur.jpg",
+    publicId: "mango-bless-cake_twurur",
     price: 21.75,
     quantity: 3,
 
@@ -64,152 +78,167 @@ export const cakesData = [
   {
     title: "Blueberry Cheesecake",
     description: "Creamy cheesecake topped with blueberry sauce.",
+    coverImage:
+      "https://res.cloudinary.com/dz6gmlrjf/image/upload/v1783604514/blueberry-chease-cake_mizdtp.jpg",
+    publicId: "blueberry-chease-cake_mizdtp",
     price: 31.5,
     quantity: 2,
-
     isAvailable: true,
-
     userId: "793b6d92-a04c-46c8-ba91-798dc73eac9f",
   },
 
   {
     title: "Caramel Crunch Cake",
     description: "Caramel flavored cake with crunchy nut toppings.",
+    coverImage:
+      "https://res.cloudinary.com/dz6gmlrjf/image/upload/v1783604512/caramel-crunch-cake_y77xqd.jpg",
+    publicId: "caramel-crunch-cake_y77xqd",
     price: 26.8,
     quantity: 5,
-
     isAvailable: true,
-
     userId: "793b6d92-a04c-46c8-ba91-798dc73eac9f",
   },
 
   {
     title: "Coffee Mocha Cake",
     description: "Mocha flavored sponge with coffee cream frosting.",
+    coverImage:
+      "https://res.cloudinary.com/dz6gmlrjf/image/upload/v1783604511/coffe-monch-cake_nizjnr.jpg",
+    publicId: "coffe-monch-cake_nizjnr",
     price: 23.4,
     quantity: 4,
-
     isAvailable: true,
-
     userId: "7b3d4417-4a3c-4051-a81b-0cba86d13397",
   },
 
   {
     title: "Pineapple Paradise Cake",
     description: "Light pineapple cake topped with fresh pineapple.",
+    coverImage:
+      "https://res.cloudinary.com/dz6gmlrjf/image/upload/v1783604512/pineapple-paradise-cake_lnbwon.jpg",
+    publicId: "pineapple-paradise-cake_lnbwon",
     price: 20.99,
     quantity: 3,
-
     isAvailable: true,
-
     userId: "7b3d4417-4a3c-4051-a81b-0cba86d13397",
   },
 
   {
     title: "Oreo Chocolate Cake",
     description: "Chocolate cake loaded with Oreo cookies and cream.",
+    coverImage:
+      "https://res.cloudinary.com/dz6gmlrjf/image/upload/v1783604510/oreo-chocklate-cake_ug5rs0.jpg",
+    publicId: "oreo-chocklate-cake_ug5rs0",
     price: 28.0,
     quantity: 5,
-
     isAvailable: true,
-
     userId: "83893ca3-f410-424b-8889-efd2f931bcd2",
   },
 
   {
     title: "Lemon Zest Cake",
     description: "Refreshing lemon flavored sponge with citrus glaze.",
+    coverImage:
+      "https://res.cloudinary.com/dz6gmlrjf/image/upload/v1783604553/lemon-jest-cake_lldlur.jpg",
+    publicId: "lemon-jest-cake_lldlur",
     price: 18.5,
     quantity: 2,
-
     isAvailable: true,
-
     userId: "83893ca3-f410-424b-8889-efd2f931bcd2",
   },
 
   {
     title: "Nutella Lava Cake",
     description: "Warm chocolate lava cake filled with Nutella.",
+    coverImage:
+      "https://res.cloudinary.com/dz6gmlrjf/image/upload/v1783604509/nutella-lava-cake_xzerfw.jpg",
+    publicId: "nutella-lava-cake_xzerfw",
     price: 32.99,
     quantity: 4,
-
     isAvailable: true,
-
     userId: "aefc130d-ce20-478f-ae09-a3ff3dcd3d59",
   },
 
   {
     title: "Rainbow Birthday Cake",
     description: "Colorful layered birthday cake with buttercream.",
+    coverImage:
+      "https://res.cloudinary.com/dz6gmlrjf/image/upload/v1783604524/rainbow-birthday-cake_bqd0wh.jpg",
+    publicId: "rainbow-birthday-cake_bqd0wh",
     price: 35.0,
     quantity: 1,
-
     isAvailable: true,
-
     userId: "aefc130d-ce20-478f-ae09-a3ff3dcd3d59",
   },
 
   {
     title: "Hazelnut Crunch Cake",
     description: "Hazelnut cream cake with roasted nut toppings.",
+    coverImage:
+      "https://res.cloudinary.com/dz6gmlrjf/image/upload/v1783604507/hazlenut-crunch-cake_t75fqf.jpg",
+    publicId: "hazlenut-crunch-cake_t75fqf",
     price: 30.25,
     quantity: 2,
-
     isAvailable: true,
-
     userId: "ba0d935a-581a-466d-986f-62ea1697bf60",
   },
 
   {
     title: "Coconut Cream Cake",
     description: "Soft coconut sponge layered with cream frosting.",
+    coverImage:
+      "https://res.cloudinary.com/dz6gmlrjf/image/upload/v1783604507/coconut-cream-cake_yenq2i.jpg",
+    publicId: "coconut-cream-cake_yenq2i",
     price: 22.75,
     quantity: 3,
-
     isAvailable: true,
-
     userId: "ba0d935a-581a-466d-986f-62ea1697bf60",
   },
 
   {
     title: "Tiramisu Cake",
     description: "Italian style tiramisu cake with coffee flavor.",
+    coverImage:
+      "https://res.cloudinary.com/dz6gmlrjf/image/upload/v1783604510/tiramisu-cake_thpm2y.jpg",
+    publicId: "tiramisu-cake_thpm2y",
     price: 34.99,
     quantity: 2,
-
     isAvailable: true,
-
     userId: "dc36234d-dfe3-464a-8888-c1d750ff1d50",
   },
 
   {
     title: "Butterscotch Cake",
     description: "Creamy butterscotch cake with caramel drizzle.",
+    coverImage:
+      "https://res.cloudinary.com/dz6gmlrjf/image/upload/v1783604507/butter-scoach-cake_u1f3tg.jpg",
+    publicId: "butter-scoach-cake_u1f3tg",
     price: 24.5,
     quantity: 4,
-
     isAvailable: true,
-
     userId: "dc36234d-dfe3-464a-8888-c1d750ff1d50",
   },
 
   {
     title: "White Chocolate Cake",
     description: "Elegant white chocolate sponge with silky frosting.",
+    coverImage:
+      "https://res.cloudinary.com/dz6gmlrjf/image/upload/v1783604522/white-chocklet-cake_kpxswq.jpg",
+    publicId: "white-chocklet-cake_kpxswq",
     price: 29.49,
     quantity: 3,
-
     isAvailable: true,
-
     userId: "e7a78353-c239-434e-a989-ac921b2e3172",
   },
 
   {
     title: "Fruit Overload Cake",
     description: "Fresh fruit topped cake with mixed berry filling.",
+    coverImage:
+      "https://res.cloudinary.com/dz6gmlrjf/image/upload/v1783604507/fruit-overload-cake_tkjdpb.jpg",
+    publicId: "fruit-overload-cake_tkjdpb",
     price: 33.75,
     quantity: 2,
-
     isAvailable: true,
     userId: "e7a78353-c239-434e-a989-ac921b2e3172",
   },
